7dc5ebf5ca18fddd1f12f6530e16e9c2ec8a0ae5
Michael Bragg <m.bragg@thomsonreuters.com>
2017-09-12 17:21:45 +0100

    Adds some more test cases and returns INSOMNIA for zero case

--- src/sheep/core.clj
+++ src/sheep/core.clj
@@ -5,20 +5,25 @@
  [number]
  (set (str number)))
 
+(def insomnia "INSOMNIA")
+
 (defn sleep
  "Counting sheep: For a supplied number N, start by naming N, 2 × N, 3 × N, and so on.
  Keep track of which digits (0, 1, 2, 3, 4, 5, 6, 7, 8, and 9) has been seen at least once so far as part of any number.
  Once each of the ten digits has been seen at least once, sleep."
  [n]
  (if (zero? n)
-  (println "INSOMNIA")
+   (do
+    (println insomnia)
+    insomnia)
   (loop [results #{}
          itr 1]
    (if (= 10 (count results))
     (* n (- itr 1))
     (do
-     (let [combined-results (union results (set (str (* itr n))))]
-       (println (str itr "#") (* itr n) combined-results)
+     (let [combined-results (union results (set (str (* itr n))))
+           max (* itr n)]
+       (println (str itr "#") max combined-results)
        (recur combined-results (+ 1 itr))))))))
 
 (sleep 1692) ;;5076
--- test/sheep/core_test.clj
+++ test/sheep/core_test.clj
@@ -4,4 +4,8 @@
 
 (deftest sleep-test
   (testing "Simple inputs"
+    (is (= "INSOMNIA" (sleep 0)))
+    (is (= 10 (sleep 1)))
+    (is (= 90 (sleep 2)))
+    (is (= 110 (sleep 11)))
     (is (= 5076 (sleep 1692)))))
